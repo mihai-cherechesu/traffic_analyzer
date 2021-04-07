7c15dc31476d851b833533cdfe0bfbc69fed1a0d
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2021-04-08 00:50:39 +0300

    exporter-instaclustr: Update dockerfile, entrypoint
    Signed-off-by: Mihai Cherechesu <mihai.cherechesu@gmail.com>

--- exporter_instaclustr/docker-entrypoint.sh
+++ exporter_instaclustr/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -n "$JMX_SERVICE_URL" ]; then
-    ./wait-for "$JMX_SERVICE_URL:$LISTEN_PORT_CASSA"
+    ./wait-for -t 60 "$JMX_SERVICE_URL:$LISTEN_PORT_CASSA"
 fi
 
 exec "$@"
--- exporter_instaclustr/loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello!"
-while true; do
-    sleep 5
-done
